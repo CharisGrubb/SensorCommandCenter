d8b14a1f3fcbf1c1ae6f59c3e1138e8c7cf31f1e
Charis <charisgrubb@gmail.com>
2024-09-10 16:24:17 -0400

    added user management endpoint

--- SensorCommandCenter/API/routers/GUIInterface.py
+++ SensorCommandCenter/API/routers/GUIInterface.py
@@ -6,3 +6,9 @@ router = APIRouter()
 @router.get("/")
 async def root(theme: str = "Standard"): ###Home Web Page
     return {"message":"Hello World"}
+
+
+
+@router.get("/UserManagement")
+async def root(theme: str = "Standard"): ###Home Web Page
+    return {"message":"UserManagement"}
