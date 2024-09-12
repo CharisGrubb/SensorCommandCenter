8f53476493ca590b0e012972e33ee1a5b6220656
Charis <charisgrubb@gmail.com>
2024-09-12 14:13:17 -0400

    working through the API parameter set up, getting initial Proof of Concept test.

--- SensorCommandCenter/API/routers/users.py
+++ SensorCommandCenter/API/routers/users.py
@@ -1,8 +1,31 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+
+import uuid
+
+
+
+#################MODELS#################
+class User(BaseModel):
+    user_id: str | None = uuid.uuid4()
+    username: str
+    f_name: str
+    l_name: str 
+    e_mail: str | None = None 
+    role: str | None = "Standard User"
+
+
+####################### ENDPOINTS ###########################
 
 router = APIRouter()
-
-
 @router.get("/users/{user_id}", tags=["users"])
-async def get_details_details(user_id:str): ###User_ID should be a UUID
+async def get_user_details(user_id:str): ###User_ID should be a UUID
+    return [{"User Role":"TEST"}]
+
+@router.put("/users/{user_id}", tags=["users"])
+async def update_user_details(user_id:str, user:User): ###User_ID should be a UUID
+    return [{"User Role":"TEST"}]
+
+@router.post("/users/newUser", tags=["users"])
+async def create_user_details(user:User): 
     return [{"User Role":"TEST"}]
--- SensorCommandCenter/deployment_setup_windows.ps1
+++ SensorCommandCenter/deployment_setup_windows.ps1
@@ -1,8 +1,10 @@
 
 
+###Need to add logic to determine file location and ensure cd in the correct spot
+
 python -m venv .venv 
 .venv\Scripts\Activate.ps1 #If your powershell execution policy is set to restricted this will not work. 
 
 python -m pip install -r requirements.txt
 
-python -m main.py 
+fastapi dev main.py #Set for dev currently as testing
