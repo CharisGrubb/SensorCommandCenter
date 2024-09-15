015a14d270ac75cf1dd919e68b1eff0b4b4e647d
Charis <charisgrubb@gmail.com>
2024-09-15 12:41:46 -0400

    added user table with bitwise access. Following basic CRUD currently

--- SensorCommandCenter/Install Scripts/Database Files/SQLite (Internal)/tables.sql
+++ SensorCommandCenter/Install Scripts/Database Files/SQLite (Internal)/tables.sql
@@ -1,13 +1,18 @@
+
+
 CREATE IF NOT EXISTS Users(
     user_id text not null PRIMARY KEY--UUID
     ,username text
     ,user_f_name text
     ,user_l_name text 
-    ,user_middle_initi8al
-    CHECK(typeof("user_id") = "text" and length("user_id" <=50)) -- Check to control the length of the uuid for data integrity. UUIDs currently are about 36 characters. Allowing extra space for newer uuid types(Security scalability)
-)
-
-
+    ,user_middle_initial text --additional distinguisher for clients for cases of two users with same first and last name
+    ,access_level INT NOT NULL DEFAULT 0, --CRUD operations Binary/bitwise map. READ=1, CREATE=2,UPDATE=4,DELETE=8 
+    ,access_until datetime null, --fill in if it's a temp access, NULL if until whenever
+    ,account_type text, --Service Account, ALT Acount, AD account, internal account, etc
+    ,CHECK(typeof("user_id") = "text" and length("user_id" <=50)) -- Check to control the length of the uuid for data integrity. UUIDs currently are about 36 characters. Allowing extra space for newer uuid types(Security scalability)
+    ,CHECK(typeof("user_middle_initial") = "text" and length("user_middle_initial" <=2)) 
+  
+  ) 
 
 
 
@@ -26,12 +31,20 @@ CREATE IF NOT EXISTS Sensors(
 
 
 CREATE IF NOT EXISTS Sensor_data_points(
-    data_point_id INTEGER PRIMARY KEY, --(AKA ROWID) gives each row a unique number id (64 bit signed int)
+    data_point_id INTEGER PRIMARY KEY --(AKA ROWID) gives each row a unique number id (64 bit signed int)
     ,sensor_id text REFERENCES Sensors(Sensor_id)
     ,int_value integer NULL
     ,dec_number numeric(10,5) NULL --For temps, while real can store a floating point, the numeric type contains more precision, (similar to float vs double in java). 
     ,str_value text NULL
     ,create_date datetime -- time at which this was recorded
 
+)
 
+CREATE IF NOT EXISTS logs(
+    log_id INTEGER PRIMARY KEY
+    ,user_id text REFERENCES Users(User_ID)
+    ,Log_note text
+    ,log_type text
+    ,log_source text
+    ,Create_Date datetime DEFAULT GETDATE(), --Timestamp of action occurred
 )
