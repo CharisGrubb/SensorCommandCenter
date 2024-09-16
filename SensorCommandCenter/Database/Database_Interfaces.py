6d05053c86cfc3f8ffb7cfb20f48335cdbb7a58f
Charis <charisgrubb@gmail.com>
2024-09-16 13:38:05 -0400

    Added logging file structure added database interface file

---
+++ SensorCommandCenter/Database/Database_Interfaces.py
@@ -0,0 +1,12 @@
+import sqlite3 #Used for internal db connection
+import sqlalchemy #used for external db connection
+
+
+class InternalDBConnection():
+    
+    pass
+
+
+class ExternalDBConnection():
+
+    pass
