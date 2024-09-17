c2671b366ddf796df881062d6b6add71cb03144f
Charis <charisgrubb@gmail.com>
2024-09-17 13:45:06 -0400

    Thinking through inherantence database structure

--- SensorCommandCenter/Database/Database_Interfaces.py
+++ SensorCommandCenter/Database/Database_Interfaces.py
@@ -4,9 +4,33 @@ import sqlalchemy #used for external db connection
 
 class InternalDBConnection():
     
+    def __init__(self):
+        self.conn=None
+
+    def connect(self):
+        self.conn = sqlite3.connect("SensorCommandCenter/Database/internal_sensor_database.db")
+
+    def close_connection(self):
+        self.conn.close()
+        self.conn=None
+
+#PARENT
+class ExternalDBConnection():
+
+    def __init__(self):
+        pass
+
+    def connect(self):
+        pass 
+
+    def close_connection(self):
+        pass
+
+#Child
+class ExternalDB_MSSQL(ExternalDBConnection):
     pass
 
 
-class ExternalDBConnection():
-
+#Child
+class ExternalDB_MySQL(ExternalDBConnection):
     pass
---
+++ SensorCommandCenter/Logging/Logger.py
@@ -0,0 +1,18 @@
+
+
+
+
+class Log:
+    def __init__(self):
+        pass 
+
+    def log_to_database(self):
+        pass
+
+    #Private acting method to store log into txt file if internal database is unavailable for some reason
+    def __store_in_queue(self):
+        pass 
+
+    #Read from txt file and push to internal db
+    def __push_from_queue_to_db(self):
+        pass
